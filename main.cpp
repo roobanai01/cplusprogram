59966bab51b1fccb8241e75b11dd1c33bd62b3de
roobanai01 <154311125+roobanai01@users.noreply.github.com>
2024-01-19 21:45:00 +0530

    Class and object model program

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+class roar
+{
+public :
+        int a,b,c;
+        int multiplication();
+        int division();
+
+
+        int addition()
+{
+        cout<<"\n---Addition Program---";
+        cout<< "\nEnter the Value of A:";
+        cin>>a;
+        cout<< "Enter the Value of B:";
+        cin>>b;
+        c=a+b;
+
+        cout<<"\nThe Added Value of " <<a <<" + " <<b<< " is:"<<c<<"\n";
+}
+        int subtraction()
+{
+        cout<<"\n---Subtraction Program---";
+        cout<< "\nEnter the Value of A:";
+        cin>>a;
+        cout<< "Enter the Value of B:";
+        cin>>b;
+        c=a-b;
+        cout<<"\nThe Subtracted Value of " <<a <<" - " <<b<< " is:"<<c<<"\n";
+}
+};
+int roar :: multiplication()
+{
+    int a, b,c;
+        cout<<"\n---Multiplication Program---";
+        cout<< "\nEnter the Value of A:";
+        cin>>a;
+        cout<< "Enter the Value of B:";
+        cin>>b;
+        c=a*b;
+        cout<<"\nThe Multiplied Value of " <<a <<" * " <<b<< " is:"<<c<<"\n";
+}
+
+int roar :: division()
+{
+    int a, b, c;
+        cout<<"\n---Division Program---";
+        cout<< "\nEnter the Value of A:";
+        cin>>a;
+        cout<< "Enter the Value of B:";
+        cin>>b;
+        c=a/b;
+        cout<<"\nThe divided Value of " <<a <<" / " <<b<< " is:"<<c<<"\n";
+}
+int main()
+{
+ roar rk;
+rk.addition();
+rk.subtraction();
+rk.multiplication();
+rk.division();
+
+    return 0;
+}
+
